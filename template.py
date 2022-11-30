fc21b2dff114b3841ee85b681d137bf93c5aef35
Rilkon <58311175+Rilkon@users.noreply.github.com>
2022-11-30 20:37:43 +0100

    Preparation for AoC2022

--- template.py
+++ template.py
@@ -1,19 +1,18 @@
 import pathlib
+import sys
+
 
 def parse(puzzle_input):
-    # Parse input.
-    return [int(line) for line in puzzle_input.split()]
+    """Parse input."""
 
 
-def part1(numbers):
+def part1(data):
     """Solve part 1."""
-    for num1 in numbers:
-        for num2 in numbers:
-            if num1 < num2 and num1 + num2 == 2020:
-                return num1 * num2
 
-def part2(numbers):
-    return 0
+
+def part2(data):
+    """Solve part 2."""
+
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
@@ -24,7 +23,7 @@ def solve(puzzle_input):
 
 
 if __name__ == "__main__":
-    path = "./day01.txt"
+    path = "./dayXX.txt"
     puzzle_input = pathlib.Path(path).read_text().strip()
     solutions = solve(puzzle_input)
     print("\n".join(str(solution) for solution in solutions))
