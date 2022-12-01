b779eeb2baff5fe41aa0178b2ef806db8256b0e7
Rilkon <d.pfauntsch@gmail.com>
2022-12-01 11:42:24 +0100

    Day01 rework

--- day01/day01.py
+++ day01/day01.py
@@ -2,41 +2,37 @@ import pathlib
 import sys
 
 
-def parse(puzzle_input):
+def parse(parsed_input):
     # Parse input
-    return puzzle_input.splitlines()
+    return parsed_input.splitlines()
 
 
-def part1(elves):
+def part1(elves: list) -> int:
     # Solve part 1
-    return elves[max(elves, key=elves.get)]
+    return max(elves)
 
 
-def part2(elves):
+def part2(elves: list) -> int:
     # Solve part 2
-    top_three = sorted(elves, key=elves.get, reverse=True)[:3]
-    return sum([elves[value] for value in top_three])
+    top_three = sorted(elves, reverse=True)[:3]
+    return sum(top_three)
 
 
-def get_elve_dict(calories) -> dict:
-    # build dictionary of elves along with their caloric sums
-    elves = {}
-    counter = 1
+def get_elve_dict(calories) -> list:
+    # Build list of elves along with their caloric sums
+    elves = [0]
+    counter = 0
     for line in calories:
-
         if line == "\n" or line.strip() == "":
             counter += 1
+            elves.append(0)
         else:
-            if counter in elves.keys():
-                elves[counter] = elves[counter] + int(line)
-            else:
-                elves[counter] = int(line)
-
+            elves[counter] += int(line)
     return elves
 
 
 def solve(puzzle_input):
-    # solve the puzzle for the given input
+    # Solve the puzzle for the given input
     data = parse(puzzle_input)
     elves = get_elve_dict(data)
     solution1 = part1(elves)
