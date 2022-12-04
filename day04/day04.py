588ee253e0eb01e2da5207b5adc1cd3695aacb01
Rilkon <58311175+Rilkon@users.noreply.github.com>
2022-12-04 08:28:37 +0100

    Day04 before breakfast

---
+++ day04/day04.py
@@ -0,0 +1,43 @@
+import pathlib
+import sys
+import re
+
+
+def parse(parsedata):
+    return parsedata.splitlines()
+
+
+def part1(data):
+
+    counter = 0
+    for line in data:
+        one_min, one_max, two_min, two_max = map(int, re.findall(r"\d+", line))
+        if ( one_min >= two_min and one_max <= two_max ) or (two_min >= one_min and two_max <= one_max):
+            counter += 1
+    return counter
+
+def part2(data):
+    counter = 0
+    for line in data:
+        one_min, one_max, two_min, two_max = map(int, re.findall(r"\d+", line))
+        one = set(range(one_min, one_max+1))
+        two = set(range(two_min, two_max+1))
+        if len(one.intersection(two)) > 0:
+            counter += 1
+    return counter
+
+
+
+def solve(puzzle):
+    data = parse(puzzle)
+    solution1 = part1(data)
+    solution2 = part2(data)
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
