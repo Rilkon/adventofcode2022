904e1196fd61332b0702ef7e14dfe53d9a2e6e54
Rilko <d.pfauntsch@gmail.com>
2022-12-16 18:15:19 +0100

    Day 16

--- day16/day16.py
+++ day16/day16.py
@@ -1,7 +1,6 @@
 import pathlib
 import re
 import sys
-import heapq
 
 
 def parse(parsedata):
@@ -17,42 +16,67 @@ def parse(parsedata):
 
 
 def best_scoring_path(tunnels, flows, p2=False):
-    # Score, Valve, Minute, Opened Valves as set
-    frontier = [(0, "AA", 1, set())]
-    # Heapify the frontier https://docs.python.org/3/library/heapq.html
-    # Should in theory prioritze the value with the highest score when popping element
-    heapq.heapify(frontier)
-    # (Current_Location, Minute): Score
+    limit = 26 if p2 else 30
+    frontier = [(0, "AA", "AA", 1, set())]
     visited = {}
     best_score = 0
-    limit = 26 if p2 else 30
 
     while frontier:
-        score, current_valve, minute, open_valves = heapq.heappop(frontier)
+        score, my_valve, ele_valve, minute, open_valves = frontier.pop()
+
+        # Prevent key error and make sure comparison is false by -1 default value
+        if visited.get((my_valve, ele_valve, minute), -1) >= score:
+            continue
+
+        visited[(my_valve, ele_valve, minute)] = score
 
         # Each time the limit is reached, save the current maximum as best score
         if minute == limit:
             best_score = max(best_score, score)
             continue
 
-        # Prevent key error and make sure comparison is false by -1 default value
-        if visited.get((current_valve, minute), -1) >= score:
-            continue
-        visited[(current_valve, minute)] = score
-
         # For unopened valves with positive flow calculate the new state including that valve being open
         # Push the new state to the frontier and remove the open valve from the current iteration again
-        if flows[current_valve] > 0 and current_valve not in open_valves:
-            open_valves.add(current_valve)
-            new_score = score + sum(flows[valve] for valve in open_valves)
-            heapq.heappush(frontier, (new_score, current_valve, minute + 1, set(open_valves)))
-            open_valves.discard(current_valve)
+        if not p2:
+            # For unopened valves with positive flow calculate the new state including that valve being open
+            # Push the new state to the frontier and remove the open valve from the current iteration again
+            if flows[my_valve] > 0 and my_valve not in open_valves:
+                open_valves.add(my_valve)
+                new_score = score + sum(flows[valve] for valve in open_valves)
+                frontier.append((new_score, my_valve, ele_valve, minute + 1, set(open_valves)))
+                open_valves.discard(my_valve)
 
-        # Valve was not opened. Calculcate the new score based on the currently open valves and a new state for
-        # each possible tunnel to explore from current valve
-        new_score = score + sum(flows[valve] for valve in open_valves)
-        for tunnel in tunnels[current_valve]:
-            heapq.heappush(frontier, (new_score, tunnel, minute + 1, set(open_valves)))
+            # Valve was not opened. Calculcate the new score based on the currently open valves and a new state for
+            # each possible tunnel to explore from current valve
+            new_score = score + sum(flows[valve] for valve in open_valves)
+            for tunnel in tunnels[my_valve]:
+                frontier.append((new_score, tunnel, ele_valve, minute + 1, set(open_valves)))
+
+        else:
+            if flows[my_valve] > 0 and my_valve not in open_valves:
+                open_valves.add(my_valve)
+
+                if flows[ele_valve] > 0 and ele_valve not in open_valves:
+                    open_valves.add(ele_valve)
+                    new_score = score + sum(flows[valve] for valve in open_valves)
+                    frontier.append((new_score, my_valve, ele_valve, minute + 1, set(open_valves)))
+                    open_valves.discard(ele_valve)
+
+                new_score = score + sum(flows[valve] for valve in open_valves)
+                for tunnel in tunnels[ele_valve]:
+                    frontier.append((new_score, my_valve, tunnel, minute + 1, set(open_valves)))
+
+                open_valves.discard(my_valve)
+
+            for tunnel in tunnels[my_valve]:
+                if flows[ele_valve] > 0 and ele_valve not in open_valves:
+                    open_valves.add(ele_valve)
+                    new_score = score + sum(flows[valve] for valve in open_valves)
+                    frontier.append((new_score, tunnel, ele_valve, minute + 1, set(open_valves)))
+                    open_valves.discard(ele_valve)
+                new_score = score + sum(flows[valve] for valve in open_valves)
+                for ele_tunnel in tunnels[ele_valve]:
+                    frontier.append((new_score, tunnel, ele_tunnel, minute + 1, set(open_valves)))
 
     return best_score
 
