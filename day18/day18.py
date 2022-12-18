86e0342db64d61e954ef2357f97858ec5359a868
Rilko <d.pfauntsch@gmail.com>
2022-12-18 21:07:17 +0100

    Day 18

--- day18/day18.py
+++ day18/day18.py
@@ -7,6 +7,13 @@ def parse(parsedata):
     return [tuple(map(int, line.split(','))) for line in parsedata.splitlines()]
 
 
+def part1(voxels):
+    # Total surface area = 6 (sides) * number of cubes
+    # Substract two from total for every pair/combination of cubes touching
+    touching_cubes = sum([are_cubes_touching(cube1, cube2) * 2 for cube1, cube2 in itertools.combinations(voxels, 2)])
+    return (6 * len(voxels)) - touching_cubes
+
+
 def are_cubes_touching(cube_a, cube_b):
     # Two cubes are touching if "manhattan distance"(?) equals 1 (due to 1*1*1 size)
     x1, y1, z1 = cube_a
@@ -14,15 +21,36 @@ def are_cubes_touching(cube_a, cube_b):
     return abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2) == 1
 
 
-def part1(data):
-    # Total surface area = 6 (sides) * number of cubes
-    # Substract two from total for every pair/combination of cubes touching
-    touching_cubes = sum([are_cubes_touching(cube1, cube2) * 2 for cube1, cube2 in itertools.combinations(data, 2)])
-    return (6 * len(data)) - touching_cubes
+def part2(voxels):
+    max_x = max(x for x, y, z in voxels)
+    max_y = max(y for x, y, z in voxels)
+    max_z = max(z for x, y, z in voxels)
 
+    min_x = min(x for x, y, z in voxels)
+    min_y = min(y for x, y, z in voxels)
+    min_z = min(z for x, y, z in voxels)
 
-def part2(data):
-    return ""
+    all_min = min(min_x, min_y, min_z) - 2
+    all_max = max(max_x, max_y, max_z) + 2
+
+    frontier = [[(all_min, all_min, all_min)]]
+    visited = set()
+    result = 0
+
+    while frontier:
+        path = frontier.pop(0)
+        current = path[-1]
+        x, y, z = current
+        for dx, dy, dz in [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]:
+            neighbor = x+dx, y+dy, z+dz
+            if neighbor not in visited:
+                if neighbor in voxels:
+                    result += 1
+                elif all((all_min <= i <= all_max) for i in neighbor):
+                    visited.add(neighbor)
+                    frontier.append(path + [neighbor])
+
+    return result
 
 
 def solve(puzzle_data):
@@ -33,8 +61,8 @@ def solve(puzzle_data):
 
 
 if __name__ == "__main__":
-    for path in sys.argv[1:]:
-        print(f"{path}:")
-        puzzle_input = pathlib.Path(path).read_text().strip()
+    for filepath in sys.argv[1:]:
+        print(f"{filepath}:")
+        puzzle_input = pathlib.Path(filepath).read_text().strip()
         solutions = solve(puzzle_input)
         print("\n".join(str(solution) for solution in solutions))
